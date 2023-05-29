c2286d2b73a030d2695bce3b26a802ee04380a2c
Jada_Mathele <102987102+J4d4-M4thele@users.noreply.github.com>
2023-05-29 10:16:16 +0200

    Add files via upload

---
+++ Chapter09/chapter9.js
@@ -0,0 +1,9 @@
+/***************************EXERCISE 9.3**************** */
+const output = document.querySelector('.output');
+output.textContent= 'Hello World';
+/*usetextContent instead of innerHTML*/
+output.classList.add('red');
+output.id = 'tester';
+output.style.backgroundColor = 'red';
+console.log(document.URL);
+output.textContent = document.URL;
